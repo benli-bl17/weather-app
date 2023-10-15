e0e7c49b03697cd4029ccc4c8358807d587684a3
Ben.Li <benli.bl17@gmail.com>
2023-10-15 22:59:09 +1000

    feat: Add font files and edit tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{html,js,jsx}'],
+  content: ['./src/**/*.{html,js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ['AlimamaFangYuanTiVF-Thin', 'sans'],
+      },
+    },
+  },
+  plugins: [],
+}
